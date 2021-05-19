3303102c37038ebc947f17291ec704ac31fab990
Sotirios Dimitriu <sotirios.dimitriou@gmail.com>
2021-05-19 13:58:14 +0300

    created working seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: '+44 20 7420 9324', category: "italian" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: '+44 20 7729 1888', category: "french" }
+shans_bistro = { name: "Shan’s Bistro", address: "Markgrafenstraße 36, 10117 Berlin-Mitte", phone_number: '49 (0)30-20 67 89 200', category: "belgian" }
+mutter_lustig = { name: "Mutter Lustig", address: "Müggelheimer Str. 1, 12555 Berlin-Köpenick", phone_number: '49 030 64094884', category: "italian" }
+duke = { name: "Duke", address: "Nürnberger Str. 50-55, 10789 Berlin, Germany", phone_number: '+49 (0)30 68 315-0', category: "japanese" }
+
+[dishoom, pizza_east, shans_bistro, mutter_lustig, duke].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
